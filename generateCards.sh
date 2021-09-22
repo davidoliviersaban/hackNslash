ca7b6b770645272d37a751e4f91fc1ae418969c1
DOS <dsaban@amadeus.com>
2021-09-22 14:41:56 +0200

    Adding capacity to manage floors. Testing Hexa vs Square configurations

--- generateCards.sh
+++ generateCards.sh
@@ -1,5 +1,7 @@
 
 rm -rf _output* _terrain*
 ruby src/ruby/deck.rb
+ruby src/ruby/tiles.rb
 cd ../printableCardsAppender
 ./gradlew appendCard --args="'../hack N slash/_output' '../hack N slash/imagesToPrint/cards'   A4 false"
+./gradlew appendCard --args="'../hack N slash/_tiles' '../hack N slash/imagesToPrint/tiles'   A4 false"
--- src/ruby/tiles.rb
+++ src/ruby/tiles.rb
@@ -1,27 +1,59 @@
 require 'squib'
 
 #deck = Squib.csv file: 'src/resources/land_cards.csv'
-deck = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+deck = ["floor1.png", "floor2.png", "floor3.png"]
+floor_size = "101mm"
 
-def drawCutlines(deck, dirname)  
+def drawHexagonalGrid(deck, dirname, floor_size)  
   ix = inches(1)
   iy = inches(0.866)
-  radius = inches(1)
+  stroke_width= 10
+  radius = inches(0.5)
+
+  png file: deck.map{|str| "src/resources/images/"+str}, width: floor_size, height: floor_size
+
   line x1: 0, y1: 0, x2: 40, y2:0, stroke_color: :red
   line x1: 0, y1: 0, x2: 0, y2:40, stroke_color: :red
   
-  for j in 0..5 do
-    for i in 0..3 do
-      polygon x: ix+(3*i)*ix, y:iy+2*j*iy, n: 6, radius: radius, stroke_color: :red
-      polygon x: ix+(3*i+1.5)*ix, y:iy+iy+2*j*iy, n: 6, radius: radius, stroke_color: :red
+  for j in 0..10 do
+    for i in 0..5 do
+      polygon x: (i*3)*radius-radius/2, y:j*iy, n: 6, radius: radius, stroke_color: :red, stroke_width: stroke_width
+      polygon x: (3*i+1.5)*radius-radius/2, y:j*iy+iy/2, n: 6, radius: radius, stroke_color: :red, stroke_width: stroke_width
     end
   end
-  save_png prefix: deck.map{|str| str.to_s+"."}, dir: dirname
+  save_png prefix: deck.map{|str| "hex."+str}, dir: dirname
 end
 
-Squib::Deck.new(cards: 1,
-#                layout: 'src/resources/tiles-layout.yml',
-                width: "210mm", height: "210mm") do # height = width*sqrt(3)/2
+def drawSquareGrid(deck, dirname, floor_size)  
+  ix = inches(1)
+  iy = inches(1)
+  radius = inches(0.5)
+  stroke_width= 10
 
-  drawCutlines(deck, '_tiles')
+  png file: deck.map{|str| "src/resources/images/"+str}, width: floor_size, height: floor_size
+
+  line x1: 0, y1: 0, x2: 40, y2:0, stroke_color: :red
+  line x1: 0, y1: 0, x2: 0, y2:40, stroke_color: :red
+  
+  for j in 0..10 do
+    for i in 0..10 do
+      polygon x: i*ix+ix/2, y:j*iy+iy/2, n: 4, radius: radius, stroke_color: :red, stroke_width: stroke_width
+    end
+  end
+  save_png prefix: deck.map{|str| "square."+str}, dir: dirname
+end
+
+Squib::Deck.new(cards: deck.size,
+#                layout: 'src/resources/tiles-layout.yml',
+                width: floor_size, height: floor_size) do # height = width*sqrt(3)/2
+
+    drawHexagonalGrid(deck, '_tiles', floor_size)
+end
+
+
+Squib::Deck.new(cards: deck.size,
+#                layout: 'src/resources/tiles-layout.yml',
+                width: floor_size, height: floor_size) do # height = width*sqrt(3)/2
+
+    drawSquareGrid(deck, '_tiles', floor_size)
 end
